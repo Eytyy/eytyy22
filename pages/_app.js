3f57c3c0c56ae1f36317c7e9e3d314809387857e
Eytyy <e.tayyem@gmail.com>
2022-05-31 01:07:13 +0300

    Add globalstyles and local fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import {ThemeProvider} from 'theme-ui';
 import theme from '@lib/theme';
+import GlobalStyles from '@lib/global.styles';
 
 function MyApp({Component, pageProps}) {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <Component {...pageProps} />
     </ThemeProvider>
   );
