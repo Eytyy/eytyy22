d0b40ca4e21abd951fb68c0782ebe80fcb126330
Eytyy <e.tayyem@gmail.com>
2022-06-07 01:47:27 +0300

    Update grid styles

--- components/header.js
+++ components/header.js
@@ -5,30 +5,31 @@ import {MdOutlineHistoryToggleOff} from 'react-icons/md';
 const Header = ({toggleProgress}) => {
   return (
     <div sx={{position: 'sticky', width: '100%', top: '0', zIndex: 10}}>
-      <div
-        sx={{
-          variant: 'wrapper.fixed',
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          px: [6, 7, 8],
-          py: [6],
-        }}
-      >
-        <Link href='/' passHref>
-          <a
-            sx={{
-              fontFamily: 'heading',
-              textDecoration: 'none',
-              color: 'accent',
-            }}
-          >
-            eytyy
-          </a>
-        </Link>
-        {/* <div onClick={toggleProgress}>
+      <div sx={{variant: 'fullGrid'}}>
+        <div
+          sx={{
+            variant: 'fullGrid.fullWidthNoBleedCol',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            py: [6],
+          }}
+        >
+          <Link href='/' passHref>
+            <a
+              sx={{
+                fontFamily: 'heading',
+                textDecoration: 'none',
+                color: 'accent',
+              }}
+            >
+              eytyy
+            </a>
+          </Link>
+          {/* <div onClick={toggleProgress}>
           <MdOutlineHistoryToggleOff />
         </div> */}
+        </div>
       </div>
     </div>
   );
